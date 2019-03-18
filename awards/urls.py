189cfd60c25dcb45eafebf6cc18caf20b8560fb9
wanjikuciku <lorna.wanjiku56@gmail.com>
2019-03-18 12:34:12 +0300

    modified

--- awards/urls.py
+++ awards/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     url(r'',include('projects.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^logout/$',views.logout,{'next_page':'/'}),
+    url(r'^api-auth/', include('rest_framework.urls')),
 ]
