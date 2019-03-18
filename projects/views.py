c0b543c9d286dc7ca394435c76f918848beb53e5
wanjikuciku <lorna.wanjiku56@gmail.com>
2019-03-18 23:42:46 +0300

    add changes

--- projects/views.py
+++ projects/views.py
@@ -63,7 +63,7 @@ def search_project(request):
     try:
         if 'project' in request.GET and request.GET['project']:
             searched_term = (request.GET.get('project')).title()
-            searched_project = Project.objects.get(project_title__icontains = searched_term.title())
+            searched_project = Project.objects.get(title__icontains = searched_term.title())
             return render(request,'search.html',{'project':searched_project})
     except (ValueError,Project.DoesNotExist):
         raise Http404()
